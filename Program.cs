49ebcfcefaf5b5713af1c6814157185b78cf20e1
krisi <pjkd5g7554@privaterelay.appleid.com>
2026-07-02 11:55:10 +0300

    create Menu

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Person> people = LoadPeopleFromFile();
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("---Избери операция---");
+                Console.WriteLine("1. CREATE (Добавяне на човек)");
+                Console.WriteLine("2. READ (Показване на всички)");
+                Console.WriteLine("3. UPDATE (Промяна на заплатата)");
+                Console.WriteLine("4. DELETE (Изтриване по име)");
+                Console.WriteLine("5. Изход");
+                Console.Write("Избор: ");
+
+                string choice=Console.ReadLine();
+                Console.WriteLine();
+                
+
+
+            }
 
 
 
